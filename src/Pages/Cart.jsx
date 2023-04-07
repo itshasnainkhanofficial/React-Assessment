def58e0a850d7d8386a44f14c9acdab717fe4cd9
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-04-07 21:34:27 +0500

    module 14 completed

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,8 +1,46 @@
+import { useContext } from "react";
+import { Context } from "../state/Context";
+import styles from "./styles/cart.module.css";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { addToCart, cartItems, setCartItems } = useContext(Context);
 
-export default Cart
+  const clickHandler = (data) => {
+    const isInCart = cartItems.find((item) => item.id === data.id);
+    if (isInCart) {
+      setCartItems((prev) => prev.filter((item) => item.id !== data.id));
+    } else {
+      addToCart(data);
+    }
+  };
+
+
+
+  return (
+    <div className={styles.container}>
+      {cartItems.length > 0 ? (
+        cartItems.map((item) => (
+          <div key={item.id} className={styles.itemContainer}>
+           
+            <div>$5.99</div>
+            <div>
+              <RiDeleteBin6Fill onClick={ () => clickHandler(item)}/>
+            </div>
+            <img src={item.url} />
+          </div>
+        ))
+      ) : (
+        <div>No cart item found</div>
+      )}
+
+      {cartItems.length > 0 ? (
+        <p className={styles.total}>
+          Total Price: ${Math.round(cartItems.length * 5.99)}
+        </p>
+      ) : null}
+    </div>
+  );
+};
+
+export default Cart;
